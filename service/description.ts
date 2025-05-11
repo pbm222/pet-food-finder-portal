4c92eb64f8848ffee5d2cffffc764ce1ddf04063
innatisina <ag1894@student.jamk.fi>
2025-05-11 21:16:07 +0300

    PFF-6 replace env variable with defined const

--- service/description.ts
+++ service/description.ts
@@ -1,7 +1,7 @@
-import axios from "axios";
+import api from "@/utils/axios";
 
 export const getDescriptionsForProduct = async (productId: string) => {
-    return axios.get(process.env.NEXT_PUBLIC_PET_FOOD_FINDER_API_URL + `/descriptions`, { params: { productId: productId } })
+    return api.get(`/descriptions`, { params: { productId: productId } })
         .then((response) => {
             return response.data;
         })
--- service/retailer.ts
+++ service/retailer.ts
@@ -1,7 +1,7 @@
-import axios from "axios";
+import api from "@/utils/axios";
 
 export const getRetailersForProduct = async (productId: string) => {
-    return axios.get(process.env.NEXT_PUBLIC_PET_FOOD_FINDER_API_URL + `/retailers`, { params: { productId: productId } })
+    return api.get(`/retailers`, { params: { productId: productId } })
         .then((response) => {
             return response.data;
         })
